c41569a487f4e9442005842027aad8d79afecc55
oleksandr kulchytskyi <itser1106@gmail.com>
2026-07-02 14:10:16 +0300

    add AppIcon AppleStore and GooglePlay modules

---
+++ app/Modules/AppIcon/app/Providers/AppIconServiceProvider.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace Modules\AppIcon\Providers;
+
+use Nwidart\Modules\Support\ModuleServiceProvider;
+use Illuminate\Console\Scheduling\Schedule;
+
+class AppIconServiceProvider extends ModuleServiceProvider
+{
+    /**
+     * The name of the module.
+     */
+    protected string $name = 'AppIcon';
+
+    /**
+     * The lowercase version of the module name.
+     */
+    protected string $nameLower = 'appicon';
+
+    /**
+     * Command classes to register.
+     *
+     * @var string[]
+     */
+    // protected array $commands = [];
+
+    /**
+     * Provider classes to register.
+     *
+     * @var string[]
+     */
+    protected array $providers = [
+        EventServiceProvider::class,
+        RouteServiceProvider::class,
+    ];
+
+    /**
+     * Define module schedules.
+     * 
+     * @param $schedule
+     */
+    // protected function configureSchedules(Schedule $schedule): void
+    // {
+    //     $schedule->command('inspire')->hourly();
+    // }
+}
